6728ef54689a6effc38cc17d091ccd233583e55a
CaseyH256 <156718039+CaseyH256@users.noreply.github.com>
2025-12-24 12:54:56 -0600

    Delete electron/preload.js

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFileDialog: (options) => ipcRenderer.invoke('open-file-dialog', options),
-  saveFile: (filePath, content) => ipcRenderer.invoke('save-file', filePath, content),
-});
